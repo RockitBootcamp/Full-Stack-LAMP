adefd6d2a1e0dfa14e845b3e763010d0f3b401e6
kfaultner <kfaultner@gmail.com>
2015-03-31 11:05:20 -0700

    adding to schema and m5 project readme

--- Milestones/m4/POS System/schema.sql
+++ Milestones/m4/POS System/schema.sql
@@ -42,13 +42,102 @@ CREATE TABLE invoice_item (
 --
 -- Insert sample data
 --
+--
 
 INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('kat', 'jacobs', 'k@jacobs.com', 'female', CURDATE());
+    VALUES ('Kat', 'Jacobs', 'k@jacobs.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Mark', 'Ackerman', 'mark@yahoo.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Jack', 'Brasher', 'jackbrash@hotmail.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('George', 'Curtis', 'gcurtis@yahoo.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Audrey', 'Hunter', 'ahunt87@hotmail.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Emily', 'Seaver', 'emseaver@test.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Madilyn', 'Reynolds', 'maddie@hotmail.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Nathan', 'Cooper', 'natecoop@gmail.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Karen', 'Tanner', 'ktanns@gmail.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Corey', 'Wilson', 'cocowils@gmail.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Amy', 'Johnson', 'ajgirl39@yahoo.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Tracey', 'Covington', 'tcove78@yahoo.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Alex', 'Tipton', 'atipto32@hotmail.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Hannah', 'Vincent', 'hvhiker@yahoo.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Connie', 'Maxwell', 'maxcon342@yahoo.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Ryan', 'Underwood', 'ryguy34@hotmail.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Jerome', 'Goodwell', 'jergood@gmail.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Adam', 'Johnson', 'adjoh98@hotmail.com', 'male', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Sandy', 'Walters', 'sandwalt29@gmail.com', 'female', CURDATE());
+
+INSERT INTO customer (first_name, last_name, email, gender, customer_since)
+    VALUES ('Brittney', 'Smith', 'britsmit78@cox.net', 'female', CURDATE());
+
+--
+-- Insert data into item table
+--
+INSERT INTO item (name, price) VALUES ('hammer', 17.95);
+
+INSERT INTO item (name, price) VALUES ('screws', 3.95);
+
+INSERT INTO item (name, price) VALUES ('drill', 67.95);
+
+INSERT INTO item (name, price) VALUES ('saw', 37.99);
+
+INSERT INTO item (name, price) VALUES ('wrench', 7.95);
+
+INSERT INTO item (name, price) VALUES ('ladder', 77.99);
+
+INSERT INTO item (name, price) VALUES ('shovel', 27.95);
 
 INSERT INTO item (name, price) VALUES ('hammer', 17.95);
 
+INSERT INTO item (name, price) VALUES ('hammer', 17.95);
+
+INSERT INTO item (name, price) VALUES ('hammer', 17.95);
+
+INSERT INTO item (name, price) VALUES ('hammer', 17.95);
+
+--
+-- Insert data into invoice table
+--
+
 INSERT INTO invoice (customer_id, created_at) VALUES (1, NOW());
+
+--
+-- Insert data into invoice_item table
+--
 
 INSERT INTO invoice_item (invoice_id, item_id, quantity) VALUES (1, 1, 13);
 
---
+++ Milestones/m5/Project/README.md
@@ -0,0 +1,11 @@
+# Milestone 5: PHP Project
+###A POS System on Laravel
+
+## Project Background and Description
+For this milestone, we will be taking the POS system you created in milestone 4 and change it to use Laravel framework. 
+
+## Project Scope
+
+##Acceptance Criteria
+
+##Tips
