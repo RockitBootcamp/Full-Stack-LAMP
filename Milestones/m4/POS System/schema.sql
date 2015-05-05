04172f5f7ec22c4c7da00e492dc076da191bf31d
Brad Westfall <brad@azpixels.com>
2015-05-05 07:51:23 -0700

    pos

--- Milestones/m4/POS System/schema.sql
+++ Milestones/m4/POS System/schema.sql
@@ -1,143 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10
+-- http://www.phpmyadmin.net
 --
--- To use this file do either of:
--- - copy/paste all the following code into the mysql client (in terminal)
--- - $ cat schema.sql | mysql -u root
+-- Host: localhost
+-- Generation Time: May 05, 2015 at 02:46 PM
+-- Server version: 5.6.15
+-- PHP Version: 5.5.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pos`
 --
 
-DROP DATABASE IF EXISTS pos;
-CREATE DATABASE pos;
-USE pos;
+-- --------------------------------------------------------
 
 --
--- Create database tables
+-- Table structure for table `customer`
 --
-CREATE TABLE customer (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(255),
-    gender VARCHAR(50),
-    customer_since DATE
-);
 
-CREATE TABLE item (
-    id INT auto_increment primary key,
-    name VARCHAR(100),
-    price DECIMAL(7,2)
-);
+CREATE TABLE IF NOT EXISTS `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `customer_since` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
-CREATE TABLE invoice (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT NOT NULL,
-    created_at TIMESTAMP
-);
+--
+-- Dumping data for table `customer`
+--
 
-CREATE TABLE invoice_item (
-  invoice_id int NOT NULL,
-  item_id int NOT NULL,
-  quantity int DEFAULT NULL,
+INSERT INTO `customer` (`id`, `first_name`, `last_name`, `email`, `gender`, `customer_since`) VALUES
+(1, 'Dave', 'Smith', 'dave@smith.com', 'm', '2015-05-04 21:53:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE IF NOT EXISTS `invoice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `invoice`
+--
+
+INSERT INTO `invoice` (`id`, `customer_id`, `created_on`) VALUES
+(1, 1, '2015-05-01 18:09:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_item`
+--
+
+CREATE TABLE IF NOT EXISTS `invoice_item` (
+  `invoice_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
   PRIMARY KEY (`invoice_id`,`item_id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Insert sample data
---
---
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Kat', 'Jacobs', 'k@jacobs.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Mark', 'Ackerman', 'mark@yahoo.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Jack', 'Brasher', 'jackbrash@hotmail.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('George', 'Curtis', 'gcurtis@yahoo.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Audrey', 'Hunter', 'ahunt87@hotmail.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Emily', 'Seaver', 'emseaver@test.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Madilyn', 'Reynolds', 'maddie@hotmail.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Nathan', 'Cooper', 'natecoop@gmail.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Karen', 'Tanner', 'ktanns@gmail.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Corey', 'Wilson', 'cocowils@gmail.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Amy', 'Johnson', 'ajgirl39@yahoo.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Tracey', 'Covington', 'tcove78@yahoo.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Alex', 'Tipton', 'atipto32@hotmail.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Hannah', 'Vincent', 'hvhiker@yahoo.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Connie', 'Maxwell', 'maxcon342@yahoo.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Ryan', 'Underwood', 'ryguy34@hotmail.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Jerome', 'Goodwell', 'jergood@gmail.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Adam', 'Johnson', 'adjoh98@hotmail.com', 'male', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Sandy', 'Walters', 'sandwalt29@gmail.com', 'female', CURDATE());
-
-INSERT INTO customer (first_name, last_name, email, gender, customer_since)
-    VALUES ('Brittney', 'Smith', 'britsmit78@cox.net', 'female', CURDATE());
-
---
--- Insert data into item table
---
-INSERT INTO item (name, price) VALUES ('hammer', 17.95);
-
-INSERT INTO item (name, price) VALUES ('screws', 3.95);
-
-INSERT INTO item (name, price) VALUES ('drill', 67.95);
-
-INSERT INTO item (name, price) VALUES ('saw', 37.99);
-
-INSERT INTO item (name, price) VALUES ('wrench', 7.95);
-
-INSERT INTO item (name, price) VALUES ('ladder', 77.99);
-
-INSERT INTO item (name, price) VALUES ('shovel', 27.95);
-
-INSERT INTO item (name, price) VALUES ('pry bar', 19.97);
-
-INSERT INTO item (name, price) VALUES ('chisel', 10.46);
-
-INSERT INTO item (name, price) VALUES ('voltage tester', 18.97);
-
-INSERT INTO item (name, price) VALUES ('laser level', 49.97);
-
---
--- Insert data into invoice table
+-- Dumping data for table `invoice_item`
 --
 
-INSERT INTO invoice (customer_id, created_at) VALUES (1, NOW());
+INSERT INTO `invoice_item` (`invoice_id`, `item_id`, `quantity`) VALUES
+(1, 1, 13);
+
+-- --------------------------------------------------------
 
 --
--- Insert data into invoice_item table
+-- Table structure for table `item`
 --
 
-INSERT INTO invoice_item (invoice_id, item_id, quantity) VALUES (1, 1, 13);
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `price` decimal(7,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`id`, `name`, `price`) VALUES
+(1, 'hammer', '17.95');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
